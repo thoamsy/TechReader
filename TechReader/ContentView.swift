087673f876758ca1f95c4036c1eafb5b9b20a617
杨魁 <yangkui.thoamsy@bytedance.com>
2020-08-31 22:42:34 +0800

    Initial Commit

---
+++ TechReader/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TechReader
+//
+//  Created by yk on 2020/8/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TechReader/TechReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechReaderApp.swift
+//  TechReader
+//
+//  Created by yk on 2020/8/31.
+//
+
+import SwiftUI
+
+@main
+struct TechReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
